60d0c3a27c3c790de6fdb65f7410d2cd036381e2
talitasaez <tltsaez@gmail.com>
2023-05-02 20:06:10 -0300

    requisito 9

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -35,18 +35,12 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    salary_list = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                salary_list.append(job)
+        except (ValueError):
+            pass
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    return salary_list
